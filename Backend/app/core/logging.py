fa8ff7f61f3a1c5b4c7a42c4832b95a0780e1371
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-07-15 23:53:14 +0200

    Añadir conversor de tiempo GMT+2 al formatter del logger

--- Backend/app/core/logging.py
+++ Backend/app/core/logging.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 
 
 LOG_DIR = "./logs"
@@ -20,6 +21,7 @@ def setup_logger(name: str) -> logging.Logger:
             "[%(asctime)s] [%(levelname)s] - %(name)s - %(message)s",
             "%Y-%m-%d %H:%M:%S",
         )
+        formatter.converter = lambda _: time.gmtime(time.time() + 2 * 3600)
         file_handler.setFormatter(formatter)
 
         logger.addHandler(file_handler)
