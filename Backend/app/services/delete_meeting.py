56698aeeb515db8ffdb91b03a0313413f2cb8844
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-07-17 16:10:14 +0200

    Añadir tipado y detalle en la excepción 404 de delete meeting

--- Backend/app/services/delete_meeting.py
+++ Backend/app/services/delete_meeting.py
@@ -4,11 +4,13 @@ from sqlmodel import Session
 from ..models.meeting_model import RetrieveMeeting
 
 
-def delete_meeting_by_id(id, session: Session):
+def delete_meeting_by_id(id: int, session: Session):
     meeting = session.get(RetrieveMeeting, id)
 
     if not meeting:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Meeting not found"
+        )
 
     session.delete(meeting)
     session.commit()
