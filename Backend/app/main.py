64fca6be3895fc710df23c73759dd797c292fa1d
Esteban Suárez Calvo <114094577+estebansuarezcalvoudc@users.noreply.github.com>
2025-07-17 16:06:53 +0200

    TFG-5 Eliminar reuniones (#12)
    * Añadir endpoint para eliminar reuniones
    
    * Añadir tags a los endpoints para mejorar la documentación en swagger

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,9 +3,12 @@ from fastapi import FastAPI
 from .core.logging import setup_logger
 from .routers import meeting
 
-logger = setup_logger(__name__)
 
-app = FastAPI()
+_tags_metadata = [{"name": "Meeting", "description": "Manage meetings"}]
+
+
+app = FastAPI(title="TFG", openapi_tags=_tags_metadata)
 app.include_router(meeting.router)
 
-logger.info("Backend is up")
+_logger = setup_logger(__name__)
+_logger.info("Backend is up")
