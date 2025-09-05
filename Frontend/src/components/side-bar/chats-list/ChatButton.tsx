47eea042c9398b98af12a8d4886f5d21fbb21321
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-05 21:43:44 +0200

    Refactorizar ChatButton

--- Frontend/src/components/side-bar/chats-list/ChatButton.tsx
+++ Frontend/src/components/side-bar/chats-list/ChatButton.tsx
@@ -3,34 +3,31 @@ import { useState } from 'react'
 import ChatButtonSpan from './ChatButtonSpan'
 import ChatOptionsDropdown from './ChatOptionsDropdown'
 
+interface chatButtonProps {
+  chatTitle: string
+}
+
 export default function ChatButton({
   chatTitle: conversationName,
-}: {
-  chatTitle: string
-}) {
+}: chatButtonProps) {
   const [isDivHovered, setDivIsHovered] = useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  const classes = 'w-full rounded-xl flex items-center hover:bg-stone-200'
-
-  const openChatButton = (
-    <button
-      className="flex-1 cursor-pointer truncate rounded-l-xl py-1.5 text-left text-stone-700"
-      onClick={() => console.log('conversation button triggered')}
-    >
-      <ChatButtonSpan>{conversationName}</ChatButtonSpan>
-    </button>
-  )
 
   const shouldShowOptions = isDivHovered || isMenuOpen
 
   return (
     <div
-      className={classes}
+      className="flex w-full items-center rounded-xl hover:bg-stone-200"
       onMouseEnter={() => setDivIsHovered(true)}
       onMouseLeave={() => setDivIsHovered(false)}
     >
-      {openChatButton}
+      <button
+        className="flex-1 cursor-pointer truncate rounded-l-xl py-1.5 text-left text-stone-700"
+        onClick={() => console.log('conversation button triggered')}
+      >
+        <ChatButtonSpan>{conversationName}</ChatButtonSpan>
+      </button>
+
       {shouldShowOptions && (
         <ChatOptionsDropdown onMenuStateChange={setIsMenuOpen} />
       )}
