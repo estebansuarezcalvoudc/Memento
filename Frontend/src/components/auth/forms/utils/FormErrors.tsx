4207c81c7f2b862cc0429574d07b9f652f62b81a
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-07 21:40:22 +0200

    Utilizar un set para eliminar errores duplicados

--- Frontend/src/components/auth/forms/utils/FormErrors.tsx
+++ Frontend/src/components/auth/forms/utils/FormErrors.tsx
@@ -2,7 +2,7 @@ export default function FormErrors({ errors }: { errors: null | string[] }) {
   return (
     errors && (
       <ul className="font-ubuntu mt-8 rounded-lg border-red-700 bg-red-200 px-3 py-1 text-sm text-red-700">
-        {errors.map(error => (
+        {[...new Set(errors)].map(error => (
           <li key={error}>{error}</li>
         ))}
       </ul>
