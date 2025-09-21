508b49836ee4f6039a1994a440205ad4fef3d8ea
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2025-09-21 14:07:28 +0200

    Añadir dialog de prueba
    Añadir pequeño dialog de prueba en el para el menú de UploadMeetings,
    junto con la funcionalidad de mostrarlo

--- Frontend/src/components/side-bar/navigation/SidebarButtons.tsx
+++ Frontend/src/components/side-bar/navigation/SidebarButtons.tsx
@@ -1,11 +1,18 @@
+import { useRef } from 'react'
+
 import {
   meetingsImage,
   newChatImage,
   uploadMeetingsImage,
 } from '../../../assets/buttonsImages'
+import UploadFilesDialog, {
+  type UploadFilesDialogHandler,
+} from '../../upload-files/UploadFilesDialog'
 import SidebarButton from '../navigation/SidebarButton'
 
 export default function SidebarButtons() {
+  const dialogRef = useRef<UploadFilesDialogHandler>(null)
+
   const buttons = [
     { image: newChatImage, text: 'New Chat', link: 'new-chat' },
     { image: meetingsImage, text: 'My Meetings', link: '' },
@@ -16,9 +23,22 @@ export default function SidebarButtons() {
     <ul className="flex-shrink-0">
       {buttons.map(button => (
         <li key={button.text}>
-          <SidebarButton svg={button.image} text={button.text} to={button.link} />
+          <SidebarButton
+            svg={button.image}
+            text={button.text}
+            to={button.link}
+          />
         </li>
       ))}
+
+      <button
+        className="text-stone-800 cursor-pointer"
+        onClick={() => dialogRef.current?.open()}
+      >
+        show dialog
+      </button>
+
+      <UploadFilesDialog dialogRef={dialogRef} />
     </ul>
   )
 }
