cd27e5dd8f570ce272bed2b11c67e414cc41c0b8
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-02-04 00:50:54 +0100

    Eliminar línea de configuración innecesaria de vite

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['localhost', '127.0.0.1'],
     proxy: {
       '/api': {
         target: 'http://backend:8000',
