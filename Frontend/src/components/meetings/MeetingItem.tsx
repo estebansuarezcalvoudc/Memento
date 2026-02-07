6a85c66f2b052b28545fb287fbd8b65d7a61df85
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-02-07 12:43:11 +0100

    Añadir botones de editar y eliminar reuniones (sin funcionalidad)
    (cherry picked from commit 86906c9d54e120550117380c17de74c23fc6dcb6)

--- Frontend/src/components/meetings/MeetingItem.tsx
+++ Frontend/src/components/meetings/MeetingItem.tsx
@@ -1,3 +1,4 @@
+import { editImage, removeImage } from '../../assets/removeImage'
 import { type Meeting } from '../../pages/Meetings'
 
 interface MeetingItemProps {
@@ -7,15 +8,18 @@ interface MeetingItemProps {
 
 export default function MeetingItem({ meeting, index }: MeetingItemProps) {
   return (
-    <div
-      className="grid grid-cols-[150px_300px_150px] gap-16 px-4 py-3 items-center cursor-pointer transition-colors duration-150 border-b border-stone-200 hover:bg-stone-100">
+    <div className="grid grid-cols-[60px_300px_150px_auto_auto] items-center gap-6 border-b border-stone-200 px-4 py-2 transition-colors duration-150 hover:bg-stone-100">
       <span className="font-ubuntu text-sm text-stone-500">{index}</span>
       <span className="font-ubuntu text-base text-stone-800">
         {meeting.title}
       </span>
-      <span className="font-ubuntu text-sm text-stone-600">
-        {meeting.date}
-      </span>
+      <span className="font-ubuntu text-base text-stone-600">{meeting.date}</span>
+      <button className="inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg p-1 hover:bg-blue-200 hover:text-blue-700">
+        {editImage}
+      </button>
+      <button className="inline-flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg p-1 hover:bg-red-200 hover:text-red-700">
+        {removeImage}
+      </button>
     </div>
   )
 }
--- Frontend/src/components/meetings/MeetingList.tsx
+++ Frontend/src/components/meetings/MeetingList.tsx
@@ -6,8 +6,8 @@ interface MeetingsListProps {
 }
 export default function MeetingsList({ meetings }: MeetingsListProps) {
   return (
-    <div className="max-w-4xl mx-auto">
-      <div className="grid grid-cols-[150px_300px_150px] gap-16 px-4 py-3 border-b border-stone-300">
+    <div className="mx-auto w-fit">
+      <div className="grid grid-cols-[60px_300px_150px_auto_auto] gap-6 px-4 py-3 border-b border-stone-300">
         <span className="font-ubuntu text-sm tracking-wide text-stone-500 uppercase">
           #
         </span>
@@ -17,6 +17,8 @@ export default function MeetingsList({ meetings }: MeetingsListProps) {
         <span className="font-ubuntu text-sm tracking-wide text-stone-500 uppercase">
           Fecha
         </span>
+        <span></span>
+        <span></span>
       </div>
       <div className="flex flex-col">
         {meetings.map((meeting, index) => (
