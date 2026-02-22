02bafbbd6be4178edb0409929511c1b33875e842
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-02-22 21:13:32 +0100

    Solucionar bug de estilos por el cual en ProviderView el header y el toggle no estaban alineados

--- Frontend/src/components/settings/sections/SubSectionTitle.tsx
+++ Frontend/src/components/settings/sections/SubSectionTitle.tsx
@@ -1,3 +1,3 @@
 export default function SubSectionTitle({ title }: { title: string }) {
-  return <h2 className="font-ubuntu text-lg text-stone-800">{title}</h2>
+  return <h2 className="m-0 font-ubuntu text-lg text-stone-800">{title}</h2>
 }
