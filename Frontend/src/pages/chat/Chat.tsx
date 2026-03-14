93349b899b7fa7049a920f880c5b95ed7084abc6
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-03-14 16:17:05 +0100

    Mostrar correctamente el scroll-bar

--- Frontend/src/pages/chat/Chat.tsx
+++ Frontend/src/pages/chat/Chat.tsx
@@ -18,20 +18,21 @@ export default function Chat() {
   }, [messages])
 
   return (
-    <div className="flex h-screen w-full flex-col px-3">
-      <ul
-        className="my-3 flex-1 flex-col-reverse overflow-y-auto"
-        ref={chatDivRef}
-      >
-        {messages.map(({ role, content }, index) => {
-          if (role === 'user') {
-            return <UserMessage key={index} content={content} />
-          } else {
-            return <AssistantMessage key={index} content={content} />
-          }
-        })}
+    <div className="flex h-screen w-full flex-col">
+      <ul className="my-3 flex-1 overflow-y-auto" ref={chatDivRef}>
+        <div className="mx-auto px-3 md:w-full lg:w-4xl">
+          {messages.map(({ role, content }, index) => {
+            if (role === 'user') {
+              return <UserMessage key={index} content={content} />
+            } else {
+              return <AssistantMessage key={index} content={content} />
+            }
+          })}
+        </div>
       </ul>
-      <ChatInput chatId={chatId!} />
+      <div className="mx-auto w-full px-3 pb-3 sm:w-full md:w-3/4 lg:w-1/2">
+        <ChatInput chatId={chatId!} />
+      </div>
     </div>
   )
 }
