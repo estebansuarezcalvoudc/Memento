3610c0085dd4f969c63e51a09bff20f84edba5c7
Esteban Suárez Calvo <suarezcalvoesteban@gmail.com>
2026-03-16 19:40:55 +0100

    Ajustar vistas de login y signup

--- Frontend/src/components/auth/AuthPageLayout.tsx
+++ Frontend/src/components/auth/AuthPageLayout.tsx
@@ -18,14 +18,17 @@ export default function AuthPageLayout({
 }: AuthPageLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center align-middle">
-      <div className="rounded-xl border-1 border-stone-400 p-8 dark:border-stone-600">
-        <h2 className="font-dongle text-center text-4xl text-stone-700 dark:text-stone-200">
+      <div className="w-2xs rounded-xl border-1 border-stone-400 px-4 py-6 dark:border-stone-600">
+        <h2 className="font-ubuntu text-center text-2xl text-stone-700 dark:text-stone-200">
           {title}
         </h2>
         {children}
         <span className="font-ubuntu mt-5 block text-center text-base text-stone-800 dark:text-stone-200">
           {`${footerText} `}
-          <Link to={linkTo} className="text-blue-700 underline">
+          <Link
+            to={linkTo}
+            className="text-blue-700 underline dark:text-blue-400"
+          >
             {linkText}
           </Link>
         </span>
